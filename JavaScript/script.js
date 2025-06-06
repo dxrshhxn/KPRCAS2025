8078fc1aee330167b3926d4005ab08f86760bcce
dxrshhxn <darshan.26k24@kprcas.ac.in>
2025-06-06 16:38:27 +0530

    java script completed

---
+++ JavaScript/script.js
@@ -0,0 +1,30 @@
+// //Odd or Even
+// var number =10;
+// if(number % 2==0){
+//     console.log("even")
+// }
+// var sum =(a,b)=>{
+//     return a+b;
+// }
+// console.log(sum(10,20))
+// var arr = [10,20,30,40];
+// var arr2 = [...arr,50,60,70];
+// console.log(arr)
+// console.log(arr2)
+
+// var marks = [90,100,89,78,99];
+// var [m1,m2,m3,m4,m5]= marks;
+// console.log(m1)
+// console.log(m2)
+// console.log(m3)
+// console.log(m4)
+// console.log(m5)
+
+// var a=10
+// var b=20
+// var sum=a+b;
+// console.log(a,"+",b,"=",sum)
+// console.log(`${a}+${b}=${sum}`)
+
+var choice=['rock','paper','scisser']
+var computerchoice = choice
